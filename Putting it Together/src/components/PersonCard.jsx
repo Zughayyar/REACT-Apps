ebe7ba6734ee8957920cc977e7d5e32a32e8a5af
Anas Zughayyar <anas.ezzughayyar@gmail.com>
2025-01-19 20:58:59 +0200

    update

---
+++ Putting it Together/src/components/PersonCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './PersonCard.css';
+
+const PersonCard = (props) => {
+    const {firstName, lastName, age, hairColor} = props;
+    return (
+        <div className="person-card">
+            <h2>{lastName}, {firstName}</h2>
+            <p>Age: {age}</p>
+            <p>Hair Color: {hairColor}</p>
+        </div>
+    )
+}
+
+export default PersonCard
