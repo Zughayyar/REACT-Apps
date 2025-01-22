2b7b9757c8fb91e5a5432f7c7da84a5aa8e5cd7a
Anas Zughayyar <anas.ezzughayyar@gmail.com>
2025-01-22 15:49:47 +0200

    new

---
+++ to_do_list/src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {ToastContainer} from "react-bootstrap";
+import ToDoList from "./components/ToDoList.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <ToDoList />
+    </div>
+  )
+}
+
+export default App
